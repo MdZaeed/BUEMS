d4c6e51f96e34c9490244a067b5aa85557eeccb3
DESKTOP-VVCADUP\Rafee <tsmrafee1@gmail.com>
2017-04-23 02:35:00 +0600

    all sheet with edit

---
+++ BUEMS/Scripts/controller/sallarySheet.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("service-app", []);
+
+// define the app's single controller
+app.controller("appCtrl", ["$scope", "baseService", "$filter",
+
+function ($scope, baseService, $filter) {
+
+    baseService.get("/Salaries/SalarySheetAng")
+    .then(function (response) {
+        $scope.salaries = response;
+
+    });
+
+    $scope.update = function () {
+        console.log($scope.salaries);
+    }
+
+}
+]);
